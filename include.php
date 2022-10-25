8c69cde04dffc69c79192be58755f770449b8b09
Mücahit Turhan <TURHANN_98@outlook.com>
2022-10-25 22:10:21 +0300

    initial commit

---
+++ include.php
@@ -0,0 +1,8 @@
+<?php 
+
+$company_name = "Turhansofttech Software Solutions ... "; 
+$slogan = "Open Source is King ! 2022";
+
+?> 
+
+ 
---
+++ index.php
@@ -0,0 +1,44 @@
+<?php include("variables.php"); ?> 
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Turhansofttech Software Solutions</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  </head>
+
+  <body>
+    <?php include("navbar.php"); ?> 
+    <br/>
+<center>
+<div class="jumbotron">
+  <h1 class="display-4">The best open source graphic design tools .</h1>
+  <p class="lead">Gimp , inkscape , scribus reviews .</p>
+  <hr class="my-4">
+  <p>GIMP is a free and open-source raster graphics editor used for image manipulation and image editing,</p>
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="turhansofttech.com" role="button">turhansofttech</a>
+  </p>
+</div>
+
+
+
+
+
+<?php include("footer.php"); ?>
+</center>
+<br/> 
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+
+
+
+
